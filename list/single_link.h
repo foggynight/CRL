8948ea190006222d3db8b8d697c6f581c8c32d5a
foggynight <foggynight709@gmail.com>
2020-12-16 17:04:09 -0330

    Add sl_add_node

--- list/single_link.h
+++ list/single_link.h
@@ -22,26 +22,52 @@ typedef struct sl_node {
 sl_node_t *sl_create_node(void);
 
 /** sl_destroy_node: Destroy a singly linked list node.
- * @param target {sl_node_t *}: Pointer to the target node */
-void sl_destroy_node(sl_node_t *target);
+ * @param node {sl_node_t *}: Pointer to the target node */
+void sl_destroy_node(sl_node_t *node);
+
+/** sl_add_node: Add a node to a singly linked list.
+ * @param head {sl_node_t **}: Pointer to a pointer to the head of the list
+ * @param tail {sl_node_t **}: Pointer to a pointer to the tail of the list
+ * @param node {sl_node_t *}:  Pointer to the node to add to the list */
+void sl_add_node(sl_node_t **head, sl_node_t **tail, sl_node_t *node);
 
 sl_node_t *sl_create_node(void)
 {
-    sl_node_t *new_node = calloc(1, sl_node_t);
-    if (!new_node) {
+    sl_node_t *node = calloc(1, sl_node_t);
+    if (!node) {
         fputs("list/single_link.h: Error: create_node failed\n", stderr);
-        exit(1);
+        exit(EXIT_FAILED);
     }
-    return new_node;
+    return node;
 }
 
-void sl_destroy_node(sl_node_t *target)
+void sl_destroy_node(sl_node_t *node)
 {
-    if (!target) {
+    if (!node) {
         fputs("list/single_link.h: Error: destroy_node failed\n", stderr);
-        exit(1);
+        exit(EXIT_FAILED);
     }
-    free(target);
+    free(node);
+}
+
+void sl_add_node(sl_node_t **head, sl_node_t **tail, sl_node_t *node)
+{
+    /* Invalid list initialization */
+    if ((*head && !*tail)
+     || (!*head && *tail) {
+        fputs("list/single_link.h: Error: Invalid list initialization\n", stderr);
+        exit(EXIT_FAILED);
+    }
+
+    /* List is empty */
+    if (!*head) {
+        *head = *tail = node;
+    }
+    /* List is not empty */
+    else {
+        (*tail)->next = node;
+        *tail = node;
+    }
 }
 
 #endif
