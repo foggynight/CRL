748d930e2fd111ceab2960ec3cc657255929d517
foggynight <foggynight709@gmail.com>
2020-12-19 03:39:57 -0330

    Add crl_log.h

---
+++ src/crl_log.h
@@ -0,0 +1,20 @@
+/* --- crl_log.h ---
+ *
+ * Logging and error handling.
+ *
+ * File Version: 0.1.0
+ * Last Updated: 2020-12-19
+ *
+ * This file is part of the crl library:
+ * https://github.com/foggynight/crl
+ *
+ * Copyright (C) 2020 Robert Coffey
+ * Released under the GPLv2 license */
+
+#ifndef CRL_LOG_H_
+#define CRL_LOG_H_
+
+#define CRL_DEFINE
+
+#endif // CRL_DEFINE
+#endif // CRL_LOG_H_
