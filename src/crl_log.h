1a8de693ef7e9674d3de7ea80b25e1b1a4906847
foggynight <foggynight709@gmail.com>
2020-12-20 08:24:52 -0330

    Add variadic logging macros

--- src/crl_log.h
+++ src/crl_log.h
@@ -2,7 +2,7 @@
  *
  * Logging and error handling.
  *
- * File Version: 0.1.10
+ * File Version: 0.2.0
  * Last Updated: 2020-12-20
  *
  * This file is part of the crl library:
@@ -14,42 +14,31 @@
 #ifndef CRL_LOG_H_
 #define CRL_LOG_H_
 
+#include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 #define CRL_LOG_INFO_ 0x1
 #define CRL_LOG_WARN_ 0x2
 #define CRL_LOG_ERR_  0x4
 
-#define crl_log(X) do { log_(0, (X)); } while (0)
-#define crl_ilog(X) do { log_(CRL_LOG_INFO_, (X)); } while (0)
-#define crl_wlog(X) do { log_(CRL_LOG_WARN_, (X)); } while (0)
-#define crl_elog(X) do { log_(CRL_LOG_ERR_, (X)); exit(EXIT_FAILURE); } while (0)
+#define crl_log(X) do { fprintf(stderr, "%s\n", (X)); } while (0)
+#define crl_ilog(X) do { fprintf(stderr, "Info: %s\n", (X)); } while (0)
+#define crl_wlog(X) do { fprintf(stderr, "Warning: %s\n", (X)); } while (0)
 
-/** log_: Print a message to stderr with an optional preamble.
- * @param flags {int}: Flags indicating preamble
- * @param msg {const char *}: Message to print */
-void log_(int flags, const char *msg);
+#define crl_logf(...) do { fprintf(stderr, __VA_ARGS__); } while (0)
+#define crl_ilogf(...) do { fprintf(stderr, "Info: " __VA_ARGS__); } while (0)
+#define crl_wlogf(...) do { fprintf(stderr, "Warning: " __VA_ARGS__); } while (0)
 
-#ifdef CRL_DEFINE
+#ifdef CRL_NO_EXIT
+#define crl_elog(X) do { fprintf(stderr, "Error: %s\n", (X)); } while (0)
+#define crl_elogf(...) do { fprintf(stderr, "Error: " __VA_ARGS__); } while (0)
+#endif // CRL_NO_EXIT
 
-void log_(int flags, const char *msg)
-{
-    switch (flags) {
-        case CRL_LOG_INFO_:
-            fprintf(stderr, "Info: %s\n", msg);
-            break;
-        case CRL_LOG_WARN_:
-            fprintf(stderr, "Warning: %s\n", msg);
-            break;
-        case CRL_LOG_ERR_:
-            fprintf(stderr, "Error: %s\n", msg);
-            break;
-        default:
-            fprintf(stderr, "%s\n", msg);
-            break;
-    }
-}
+#ifndef CRL_NO_EXIT
+#include <stdlib.h>
 
-#endif // CRL_DEFINE
+#define crl_elog(X) do { fprintf(stderr, "Error: %s\n", (X)); exit(EXIT_FAILURE); } while (0)
+#define crl_elogf(...) do { fprintf(stderr, "Error: " __VA_ARGS__); exit(EXIT_FAILURE); } while (0)
+#endif // CRL_NO_EXIT
+
 #endif // CRL_LOG_H_
