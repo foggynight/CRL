b5324508aa1f1380a8bdfdc05ed228d1c97512ce
foggynight <foggynight709@gmail.com>
2020-12-20 04:51:26 -0330

    Fix fprintf arguments

--- src/crl_log.h
+++ src/crl_log.h
@@ -2,8 +2,8 @@
  *
  * Logging and error handling.
  *
- * File Version: 0.1.5
- * Last Updated: 2020-12-19
+ * File Version: 0.1.6
+ * Last Updated: 2020-12-20
  *
  * This file is part of the crl library:
  * https://github.com/foggynight/crl
@@ -16,15 +16,15 @@
 
 #include <stdio.h>
 
-#define LOG_ERR_  1
+#define LOG_INFO_ 1
 #define LOG_WARN_ (1 < 1)
-#define LOG_INFO_ (1 < 2)
+#define LOG_ERR_  (1 < 2)
 
 #define log(X)   { log_((X), NULL); }
+#define i_log(X) { log_((X), LOG_INFO_); }
+#define w_log(X) { log_((X), LOG_WARN_); }
 #define e_log(X) { log_((X), LOG_ERR_); \
                    exit(EXIT_FAILURE); }
-#define w_log(X) { log_((X), LOG_WARN_); }
-#define i_log(X) { log_((X), LOG_INFO_); }
 
 /** log: Print a message to stderr with an optional preamble.
  * @param msg {const char *}: Message to print
@@ -36,17 +36,17 @@ void log_(const char *msg, int flags);
 void log_(const char *msg, int flags)
 {
     switch (flags) {
-        case ERR:
-            fprintf("Error: %s\n", msg, stderr);
+        case INFO:
+            fprintf(stderr, "Info: %s\n", msg);
             break;
         case WARN:
-            fprintf("Warning: %s\n", msg, stderr);
+            fprintf(stderr, "Warning: %s\n", msg);
             break;
-        case INFO:
-            fprintf("Info: %s\n", msg, stderr);
+        case ERR:
+            fprintf(stderr, "Error: %s\n", msg);
             break;
         default:
-            fprintf("%s\n", msg, stderr);
+            fprintf(stderr, "%s\n", msg);
             break;
     }
 }
