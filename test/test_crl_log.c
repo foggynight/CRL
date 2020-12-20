1b5e565c11828259a75561a272b6ec82e8698d16
foggynight <foggynight709@gmail.com>
2020-12-20 08:25:13 -0330

    Update unit tests

--- test/test_crl_log.c
+++ test/test_crl_log.c
@@ -1,18 +1,24 @@
 // Copyright (C) 2020 Robert Coffey
 // Released under the GPLv2 license
-// Target: crl_list.h v0.1.9
+// Target: crl_list.h v0.2.0
 
 #include <stdio.h>
 
-#define CRL_DEFINE
+#define CRL_NO_EXIT
 #include "../src/crl_log.h"
 
 int main(void)
 {
-    log("This is a test");
-    i_log("This is a test");
-    w_log("This is a test");
-    e_log("This is a test\n"
-          "test_crl_log.c: All tests passed");
+    crl_log("This is a test");
+    crl_ilog("This is a test");
+    crl_wlog("This is a test");
+    crl_elog("This is a test");
+
+    crl_logf("%s\n", "This is a test");
+    crl_ilogf("%s\n", "This is a test");
+    crl_wlogf("%s\n", "This is a test");
+    crl_elogf("%s\n", "This is a test");
+
+    puts("test_crl_log.c: All tests passed");
     return 0;
 }
