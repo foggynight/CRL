65a30a54cdca52ffe597a37a59bea1fde5fe602a
foggynight <foggynight709@gmail.com>
2020-12-21 19:09:53 -0330

    Update test description

--- test/test_crl_log.c
+++ test/test_crl_log.c
@@ -1,6 +1,11 @@
-/* Copyright (C) 2020 Robert Coffey
- * Released under the MIT license
- * Target: crl_list.h v0.2.x */
+/* --- test_crl_log.c ---
+ *
+ * Target: crl_log.h v0.2.x
+ * First Commit: 2020-12-20
+ * Last Updated: 2020-12-21
+ *
+ * Copyright (C) 2020 Robert Coffey
+ * Released under the MIT license */
 
 #include <stdio.h>
 
