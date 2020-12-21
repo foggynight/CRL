f6d48334247829ed1cc919a34792c69ca197739d
foggynight <foggynight709@gmail.com>
2020-12-21 20:29:32 -0330

    Align macro definitions

--- src/crl_log.h
+++ src/crl_log.h
@@ -8,7 +8,7 @@
  * This file is part of the crl library:
  * https://github.com/foggynight/crl
  *
- * File Version: 0.2.3
+ * File Version: 0.2.4
  * First Commit: 2020-12-19
  * Last Updated: 2020-12-21
  *
@@ -25,22 +25,22 @@
 #define CRL_LOG_WARN_ 0x2
 #define CRL_LOG_ERR_  0x4
 
-#define crl_log(X) do { fprintf(stderr, "%s\n", (X)); } while (0)
+#define crl_log(X)  do { fprintf(stderr, "%s\n", (X)); } while (0)
 #define crl_ilog(X) do { fprintf(stderr, "Info: %s\n", (X)); } while (0)
 #define crl_wlog(X) do { fprintf(stderr, "Warning: %s\n", (X)); } while (0)
 
-#define crl_logf(...) do { fprintf(stderr, __VA_ARGS__); } while (0)
+#define crl_logf(...)  do { fprintf(stderr, __VA_ARGS__); } while (0)
 #define crl_ilogf(...) do { fprintf(stderr, "Info: " __VA_ARGS__); } while (0)
 #define crl_wlogf(...) do { fprintf(stderr, "Warning: " __VA_ARGS__); } while (0)
 
 #ifndef CRL_NO_EXIT
 #include <stdlib.h>
-#define crl_elog(X) do { fprintf(stderr, "Error: %s\n", (X)); exit(EXIT_FAILURE); } while (0)
+#define crl_elog(X)    do { fprintf(stderr, "Error: %s\n", (X)); exit(EXIT_FAILURE); } while (0)
 #define crl_elogf(...) do { fprintf(stderr, "Error: " __VA_ARGS__); exit(EXIT_FAILURE); } while (0)
 #endif // CRL_NO_EXIT
 
 #ifdef CRL_NO_EXIT
-#define crl_elog(X) do { fprintf(stderr, "Error: %s\n", (X)); } while (0)
+#define crl_elog(X)    do { fprintf(stderr, "Error: %s\n", (X)); } while (0)
 #define crl_elogf(...) do { fprintf(stderr, "Error: " __VA_ARGS__); } while (0)
 #endif // CRL_NO_EXIT
 
