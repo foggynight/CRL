23afa3aa74b1c0ed7e689ff07ca9d8df98597fae
foggynight <foggynight709@gmail.com>
2020-12-24 07:59:19 -0330

    Update rtb_list.h unit test

--- test/test_rtb_list.c
+++ test/test_rtb_list.c
@@ -20,6 +20,11 @@
  * @param list List to fill */
 static void populate_list(sl_list_t *list);
 
+/* list_is_sorted: Check if a list is sorted in ascending order.
+ * @param list List to check
+ * @return Non-zero if list is sorted */
+static int list_is_sorted(sl_list_t *list);
+
 int main(void)
 {
     assert(NODE_COUNT >= 3); // Test cases require at least three nodes
@@ -27,31 +32,32 @@ int main(void)
     /* Test node functions */
     sl_node_t *node = sl_create_node();
     assert(node);
+    assert(!node->val && !node->next);
     node = sl_destroy_node(node);
     assert(!node);
 
     /* Create and populate list */
     sl_list_t *list = sl_create_list();
+    assert(!list->head && !list->tail);
     populate_list(list);
+    assert(list->head && list->tail);
 
     /* Test list values */
-    int expected_value = 0;
-    for (sl_node_t *walk = list->head; walk; walk = walk->next) {
-        int node_value = *(int *)walk->val;
-        assert(node_value == expected_value);
-        ++expected_value;
+    sl_node_t *walk = list->head;
+    for (int i = 0; i < NODE_COUNT; ++i) {
+        assert(walk);
+        assert(*(int *)walk->val == i);
+        walk = walk->next;
     }
 
     /* Consume the list head-first */
     for (sl_node_t *head = list->head; head; head = list->head) {
         int node_value = *(int *)head->val;
-        expected_value = *(int *)list->head->val;
-        sl_node_t *expected_head = head->next;
+        int expected_value = *(int *)list->head->val;
         assert(node_value == expected_value);
 
-        free(list->head->val);
+        sl_node_t *expected_head = head->next;
         sl_remove_node(list, list->head);
-
         assert(list->head == expected_head);
     }
     populate_list(list);
@@ -59,7 +65,7 @@ int main(void)
     /* Consume the list tail-first */
     for (sl_node_t *tail = list->tail; tail; tail = list->tail) {
         int node_value = *(int *)tail->val;
-        expected_value = *(int *)list->tail->val;
+        int expected_value = *(int *)list->tail->val;
         assert(node_value == expected_value);
 
         sl_node_t *expected_tail;
@@ -72,7 +78,6 @@ int main(void)
                  expected_tail = expected_tail->next);
          }
 
-        free(list->tail->val);
         sl_remove_node(list, list->tail);
 
         assert(list->tail == expected_tail);
@@ -81,15 +86,40 @@ int main(void)
     }
     populate_list(list);
 
-    /* Remove node in the list body */
-    free(list->head->next->val);
+    /* Remove a node in the list body */
     sl_remove_node(list, list->head->next);
     assert(list->head && list->tail);
     assert(*(int *)list->head->val + 2 == *(int *)list->head->next->val);
 
     /* Empty the list */
+    assert(list);
     sl_destroy_list(list);
+    assert(list);
     assert(!list->head && !list->tail);
+
+    /* Insert values out of order: [0, 1, 2, 3, 4] */
+    int list_values[] = {2, 0, 1, 5, 4};
+    for (int i = 0; i < 3; ++i) {
+        node = sl_create_node();
+        assert(node);
+
+        int *val = (int *)malloc(sizeof(int));
+        assert(val);
+
+        *val = list_values[i];
+        node->val = val;
+
+        switch(i) {
+            case 0: sl_insert_node(list, node, 1); break; // Insert at index greater than size of list
+            case 1: sl_insert_node(list, node, 0); break; // Insert at the beginning of the list
+            case 2: sl_insert_node(list, node, 1); break; // Insert in the list body
+            case 3: sl_insert_node(list, node, 3); break; // Insert at the end of list
+            case 4: sl_insert_node(list, node, 3); break; // Insert in the list body
+        }
+
+        /* Test list values */
+        assert(list_is_sorted(list));
+    }
 
     /* Destroy the list */
     assert(list);
@@ -117,7 +147,7 @@ static void populate_list(sl_list_t *list)
         *val = i;
 
         node->val = val;
-        sl_add_node(list, node);
+        sl_append_node(list, node);
 
         assert(list->head == original_head);
         assert(list->tail == node);
@@ -125,4 +155,15 @@ static void populate_list(sl_list_t *list)
 
     if (NODE_COUNT > 0)
         assert(list->head && list->tail);
+}
+
+static int list_is_sorted(sl_list_t *list)
+{
+    for (sl_node_t *walk = list->head; walk; walk = walk->next) {
+        if (!walk->next)
+            return 1;
+        else if (*(int *)walk->val > *(int *)walk->next->val)
+            return 0;
+    }
+    return 1;
 }
