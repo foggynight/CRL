61273a5352e81d6eebbcedbccb529a15c00c9201
foggynight <foggynight709@gmail.com>
2020-12-24 07:22:44 -0330

    Implement sl_insert_node
    - Add declarations sl_insert_node and dl_insert_node
    - Implement sl_insert_node
    - Refactor whitespace
    - Fix error messages

--- src/rtb_list.h
+++ src/rtb_list.h
@@ -10,7 +10,7 @@
  * This file is part of the rtb library:
  * https://github.com/foggynight/rtb
  *
- * File Version: 0.6.0
+ * File Version: 0.6.1
  * First Commit: 2020-12-16
  * Last Updated: 2020-12-24
  *
@@ -53,10 +53,10 @@ sl_list_t *sl_create_list(void);
  * @return Always NULL */
 sl_list_t *sl_destroy_list(sl_list_t *list);
 
-/* sl_empty_p: Check if a singly linked list is empty.
+/* sl_is_empty: Check if a singly linked list is empty.
  * @param list Pointer to the target list
  * @return Non-zero if the list is empty */
-int sl_empty_p(sl_list_t *list);
+int sl_is_empty(sl_list_t *list);
 
 /* sl_append_node: Append a node to a singly linked list.
  * @param list Pointer to the target list
@@ -66,9 +66,8 @@ void sl_append_node(sl_list_t *list, sl_node_t *node);
 /* sl_insert_node: Insert a node in a singly linked list.
  * @param list Pointer to the target list
  * @param node Pointer to the node to add
- * @param pos  Position to insert the node
- * @return Non-zero if invalid pos */
-int sl_insert_node(sl_list_t *list, sl_node_t *node, int pos);
+ * @param pos  Position to insert the node */
+void sl_insert_node(sl_list_t *list, sl_node_t *node, int pos);
 
 /* sl_remove_node: Remove a node from a singly linked list.
  * @param list Pointer to the target list
@@ -112,10 +111,10 @@ dl_list_t *dl_create_list(void);
  * @return Always NULL */
 dl_list_t *dl_destroy_list(dl_list_t *list);
 
-/* dl_empty_p: Check if a doubly linked list is empty.
+/* dl_is_empty: Check if a doubly linked list is empty.
  * @param list Pointer to the target list
  * @return Non-zero if the list is empty */
-int dl_empty_p(dl_list_t *list);
+int dl_is_empty(dl_list_t *list);
 
 /* dl_append_node: Append a node to a doubly linked list.
  * @param list Pointer to the target list
@@ -125,9 +124,8 @@ void dl_append_node(dl_list_t *list, dl_node_t *node);
 /* dl_insert_node: Insert a node in a doubly linked list.
  * @param list Pointer to the target list
  * @param node Pointer to the node to add
- * @param pos  Position to insert the node
- * @return Non-zero if invalid pos */
-int dl_insert_node(dl_list_t *list, dl_node_t *node, int pos);
+ * @param pos  Position to insert the node */
+void dl_insert_node(dl_list_t *list, dl_node_t *node, int pos);
 
 /* dl_remove_node: Remove a node from a doubly linked list.
  * @param list Pointer to the target list
@@ -158,9 +156,10 @@ sl_node_t *sl_create_node(void)
 sl_node_t *sl_destroy_node(sl_node_t *node)
 {
     if (!node) {
-        fputs("rtb_list.h: Error: sl_destroy_node: Cannot destroy NULL\n", stderr);
+        fputs("rtb_list.h: Error: sl_destroy_node: node is NULL\n", stderr);
         exit(EXIT_FAILURE);
     }
+    free(node->val);
     free(node);
     return NULL;
 }
@@ -177,19 +176,27 @@ sl_list_t *sl_create_list(void)
 
 sl_list_t *sl_destroy_list(sl_list_t *list)
 {
-    while (!sl_empty_p(list)) {
+    if (!list) {
+        fputs("rtb_list.h: Error: sl_destroy_list: list is NULL\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    while (!sl_is_empty(list)) {
         free(list->head->val);
         sl_remove_node(list, list->head);
     }
     return NULL;
 }
 
-int sl_empty_p(sl_list_t *list)
+int sl_is_empty(sl_list_t *list)
 {
+    if (!list) {
+        fputs("rtb_list.h: Error: sl_is_empty: list is NULL\n", stderr);
+        exit(EXIT_FAILURE);
+    }
     if ((list->head && !list->tail)
         || (!list->head && list->tail))
     {
-        fputs("rtb_list.h: Error: sl_empty_p: Invalid list initialization\n", stderr);
+        fputs("rtb_list.h: Error: sl_is_empty: invalid list initialization\n", stderr);
         exit(EXIT_FAILURE);
     }
     return !list->head && !list->tail;
@@ -197,12 +204,15 @@ int sl_empty_p(sl_list_t *list)
 
 void sl_append_node(sl_list_t *list, sl_node_t *node)
 {
-    if (!node) {
-        fputs("rtb_list.h: Error: sl_add_node: Cannot add NULL\n", stderr);
+    if (!list) {
+        fputs("rtb_list.h: Error: sl_append_node: list is NULL\n", stderr);
         exit(EXIT_FAILURE);
     }
-
-    if (sl_empty_p(list)) {
+    if (!node) {
+        fputs("rtb_list.h: Error: sl_append_node: node is NULL\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    if (sl_is_empty(list)) {
         list->head = list->tail = node;
     }
     else {
@@ -211,48 +221,80 @@ void sl_append_node(sl_list_t *list, sl_node_t *node)
     }
 }
 
-int sl_insert_node(sl_list_t *list, sl_node_t *node, int pos)
+void sl_insert_node(sl_list_t *list, sl_node_t *node, int pos)
 {
-
+    if (!list) {
+        fputs("rtb_list.h: Error: sl_insert_node: list is NULL\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    if (!node) {
+        fputs("rtb_list.h: Error: sl_insert_node: node is NULL\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    if (pos < 0) {
+        fputs("rtb_list.h: Error: sl_insert_node: invalid pos\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    else if (sl_is_empty(list)) {
+        list->head = list->tail = node;
+    }
+    else if (pos == 0) {
+        node->next = list->head;
+        list->head = node;
+    }
+    else {
+        int index = 0;
+        sl_node_t *last = NULL, *walk = list->head;
+        while (index < pos && walk) {
+            last = walk;
+            walk = walk->next;
+            ++index;
+        }
+        last->next = node;
+        node->next = walk;
+        if (!walk)
+            list->tail = node;
+    }
 }
 
 void sl_remove_node(sl_list_t *list, sl_node_t *node)
 {
+    if (!list) {
+        fputs("rtb_list.h: Error: sl_remove_node: list is NULL\n", stderr);
+        exit(EXIT_FAILURE);
+    }
     if (!node) {
-        fputs("rtb_list.h: Error: sl_remove_node: Cannot remove NULL\n", stderr);
+        fputs("rtb_list.h: Error: sl_remove_node: node is NULL\n", stderr);
         exit(EXIT_FAILURE);
     }
-    if (sl_empty_p(list)) {
-        fputs("rtb_list.h: Error: sl_remove_node: List is empty\n", stderr);
+    if (sl_is_empty(list)) {
+        fputs("rtb_list.h: Error: sl_remove_node: list is empty\n", stderr);
         exit(EXIT_FAILURE);
     }
-
     if (node == list->head) {
         sl_node_t *old_head = list->head;
-
         list->head = (list->head)->next;
         if (old_head == list->tail)
             list->tail = list->head;
-
         sl_destroy_node(old_head);
     }
     else if (node == list->tail) {
         sl_node_t *old_tail = list->tail;
-
         sl_node_t *walk;
-        for (walk = list->head; walk->next != list->tail; walk = walk->next);
+        for (walk = list->head;
+             walk->next != list->tail;
+             walk = walk->next);
         list->tail = walk;
         list->tail->next = NULL;
-
         sl_destroy_node(old_tail);
     }
     else {
         sl_node_t *walk;
-        for (walk = list->head; walk->next != node; walk = walk->next);
-
+        for (walk = list->head;
+             walk->next != node;
+             walk = walk->next);
         sl_node_t *new_next = walk->next->next;
         sl_destroy_node(walk->next);
-
         walk->next = new_next;
     }
 }
@@ -295,19 +337,19 @@ dl_list_t *dl_create_list(void)
 
 dl_list_t *dl_destroy_list(dl_list_t *list)
 {
-    while (!dl_empty_p(list)) {
+    while (!dl_is_empty(list)) {
         free(list->head->val);
         dl_remove_node(list, list->head);
     }
     return NULL;
 }
 
-int dl_empty_p(dl_list_t *list)
+int dl_is_empty(dl_list_t *list)
 {
     if ((list->head && !list->tail)
         || (!list->head && list->tail))
     {
-        fputs("rtb_list.h: Error: dl_empty_p: Invalid list initialization\n", stderr);
+        fputs("rtb_list.h: Error: dl_is_empty: Invalid list initialization\n", stderr);
         exit(EXIT_FAILURE);
     }
     return !list->head && !list->tail;
@@ -318,7 +360,7 @@ void dl_append_node(dl_list_t *list, dl_node_t *node)
 
 }
 
-int dl_insert_node(dl_list_t *list, dl_node_t *node, int pos)
+void dl_insert_node(dl_list_t *list, dl_node_t *node, int pos)
 {
 
 }
