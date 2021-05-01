a844124725fbfab7223efe32330915b77f6d168d
foggynight <foggynight709@gmail.com>
2021-05-01 01:41:45 -0230

    Rename size member to len and update comments

--- inc/rtb_buffer.h
+++ inc/rtb_buffer.h
@@ -14,34 +14,34 @@
 
 typedef struct rtb_buffer {
 	void **data;	// Data contained within the buffer
-	size_t size;	// Size of the buffer -- Number of blocks allocated in data
+	size_t len;	// Length of the buffer -- Number of blocks allocated in data
 	size_t end;	// Index after the last element in data -- Zero implies data is empty
 } rtb_buffer_t;
 
-rtb_buffer_t *rtb_buffer_init(size_t initial_size);
+rtb_buffer_t *rtb_buffer_init(size_t initial_len);
 void rtb_buffer_destroy(rtb_buffer_t *buffer);
 int rtb_buffer_push(rtb_buffer_t *buffer, void *element);
 void *rtb_buffer_pop(rtb_buffer_t *buffer);
 void *rtb_buffer_at(rtb_buffer_t *buffer, size_t index);
 int rtb_buffer_set(rtb_buffer_t *buffer, size_t index, void *value);
 
-#endif // RTB_BUFFER_H
+#endif	// RTB_BUFFER_H
 
 #ifdef RTB_DEFINE
 
 #include <stdlib.h>
 
-#define GROWTH_FACTOR 2	// Buffer size is multiplied by GROWTH_FACTOR on resize
+#define GROWTH_FACTOR 2	// Buffer length is multiplied by GROWTH_FACTOR on growth
 
-rtb_buffer_t *rtb_buffer_init(size_t initial_size)
+rtb_buffer_t *rtb_buffer_init(size_t initial_len)
 {
 	rtb_buffer_t *buffer = malloc(sizeof buffer);
 	if (!buffer) return NULL;
 
-	buffer->data = malloc(initial_size * sizeof(void *));
+	buffer->data = malloc(initial_len * sizeof(void *));
 	if (!buffer->data) return NULL;
 
-	buffer->size = initial_size;
+	buffer->len = initial_len;
 	buffer->end = 0;
 
 	return buffer;
@@ -55,14 +55,14 @@ void rtb_buffer_destroy(rtb_buffer_t *buffer)
 
 int rtb_buffer_push(rtb_buffer_t *buffer, void *element)
 {
-	if (buffer->end == buffer->size) {
-		size_t new_size = GROWTH_FACTOR * buffer->size;
+	if (buffer->end == buffer->len) {
+		size_t new_len = GROWTH_FACTOR * buffer->len;
 
-		buffer->data = realloc(buffer->data, new_size * sizeof(void *));
+		buffer->data = realloc(buffer->data, new_len * sizeof(void *));
 		if (!buffer->data)
 			return 0;
 
-		buffer->size = new_size;
+		buffer->len = new_len;
 	}
 
 	buffer->data[buffer->end++] = element;
@@ -94,4 +94,4 @@ int rtb_buffer_set(rtb_buffer_t *buffer, size_t index, void *value)
 	return 1;
 }
 
-#endif // RTB_DEFINE
+#endif	// RTB_DEFINE
