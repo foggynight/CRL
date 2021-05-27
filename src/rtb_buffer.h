91d59bd1fc75013b58ad78215546559d8671ca6d
foggynight <foggynight709@gmail.com>
2021-05-27 04:22:24 -0230

    rtb_buffer.h - v0.1.2
    - Rename the len member of rtb_buffer_t to size and rename associated
      function parameters from len to size
    - Assert that the new_size argument of rtb_buffer_resize must be greater
      than the current size of the given rtb_buffer_t
    - Add comments describing rtb_buffer_t and function declarations
    - Add guard to growth factor macro

--- src/rtb_buffer.h
+++ src/rtb_buffer.h
@@ -1,5 +1,5 @@
 /**
- * rtb_buffer.h - v0.1.1 - Variable sized generic buffer
+ * rtb_buffer.h - v0.1.2 - Variable sized generic buffer type
  *
  * This file is part of the rtb library.
  *
@@ -12,19 +12,115 @@
 
 #include <stddef.h>
 
+/**
+ * Variable sized generic buffer type. Implemented using a block of
+ * void pointers.
+ *
+ * @member size	Number of pointers allocated in the data buffer
+ * @member end	Index after the last element in the data buffer
+ * @member data	Data buffer containing block of pointers
+ **/
 typedef struct rtb_buffer {
-	size_t len;	// Length of the buffer -- Number of blocks allocated in data
-	size_t end;	// Index after the last element in data -- Zero implies data is empty
-	void **data;	// Data contained within the buffer
+	size_t size;
+	size_t end;
+	void **data;
 } rtb_buffer_t;
 
-rtb_buffer_t *rtb_buffer_init(size_t initial_len);
+/**
+ * Allocate an rtb_buffer_t and empty data buffer of size initial_size,
+ * then set rtb_buffer_t members to their initial values.
+ *
+ * @param initial_size	Initial size of the data buffer
+ *
+ * @return Pointer to a new rtb_buffer_t or NULL on failure
+ *
+ * @note Must be destroyed using rtb_buffer_destroy
+ **/
+rtb_buffer_t *rtb_buffer_init(size_t initial_size);
+
+/**
+ * Destroy an rtb_buffer_t, freeing the data buffer and the rtb_buffer_t
+ * itself.
+ *
+ * @param buffer	rtb_buffer_t to destroy
+ **/
 void rtb_buffer_destroy(rtb_buffer_t *buffer);
+
+/**
+ * Check if an rtb_buffer_t has an empty data buffer, represented by the
+ * end member being equal to zero.
+ *
+ * @param buffer	rtb_buffer_t to check
+ *
+ * @return Non-zero if the data buffer is empty
+ **/
 int rtb_buffer_empty(rtb_buffer_t *buffer);
+
+/**
+ * Resize the data buffer of an rtb_buffer_t.
+ *
+ * @param buffer	rtb_buffer_t to resize
+ * @param new_size	New size of the data buffer
+ *
+ * @return Non-zero on success
+ *
+ * @note new_size must be greater than current size of the data buffer
+ * @note Calling this function on an rtb_buffer_t will invalidate any
+ *	pointers to its data buffer.
+ **/
 int rtb_buffer_resize(rtb_buffer_t *buffer, size_t new_size);
+
+/**
+ * Push a pointer onto the end of the data buffer of an rtb_buffer_t,
+ * incrementing its end member.
+ *
+ * @param buffer	rtb_buffer_t containing data buffer to push onto
+ * @param element	Element to push onto the data buffer
+ *
+ * @return Non-zero on success
+ *
+ * @note Calling this function on an rtb_buffer_t may invalidate any
+ *	pointers to its data buffer.
+ **/
 int rtb_buffer_push(rtb_buffer_t *buffer, void *element);
+
+/**
+ * Pop a pointer off the end of the data buffer of an rtb_buffer_t,
+ * decrementing its end member.
+ *
+ * @param buffer	rtb_buffer_t containing data buffer to pop from
+ *
+ * @return Pointer popped off the data buffer
+ *
+ * @note The data buffer must not be empty, this can be checked for
+ *	using rtb_buffer_empty
+ **/
 void *rtb_buffer_pop(rtb_buffer_t *buffer);
+
+/**
+ * Get the element at the given index in the data buffer of an
+ * rtb_buffer_t.
+ *
+ * @param buffer	rtb_buffer_t containing data buffer to read from
+ * @param index	Index in the data buffer to read from
+ *
+ * @return Pointer stored at index in the data buffer
+ *
+ * @note index must be less than the end member of the rtb_buffer_t
+ **/
 void *rtb_buffer_at(rtb_buffer_t *buffer, size_t index);
+
+/**
+ * Set the element at the given index in the data buffer of an
+ * rtb_buffer_t.
+ *
+ * @param buffer	rtb_buffer_t containing data buffer to write to
+ * @param index	Index in the data buffer to write to
+ *
+ * @return Pointer stored at index in the data buffer
+ *
+ * @note index must be less than the end member of the rtb_buffer_t
+ **/
 void rtb_buffer_set(rtb_buffer_t *buffer, size_t index, void *value);
 
 #endif	// RTB__BUFFER_H
@@ -35,17 +131,19 @@ void rtb_buffer_set(rtb_buffer_t *buffer, size_t index, void *value);
 #include <assert.h>
 #include <stdlib.h>
 
-#define GROWTH_FACTOR 2	// Buffer length is multiplied by GROWTH_FACTOR on growth
+// The size of the data buffer of an rtb_buffer_t is multiplied by
+// RTB__GROWTH_FACTOR when pushing onto an rtb_buffer_t at max capacity.
+#define RTB__GROWTH_FACTOR 2
 
-rtb_buffer_t *rtb_buffer_init(size_t initial_len)
+rtb_buffer_t *rtb_buffer_init(size_t initial_size)
 {
 	rtb_buffer_t *buffer = malloc(sizeof buffer);
 	if (!buffer) return NULL;
 
-	buffer->data = malloc(initial_len * sizeof(void *));
+	buffer->data = malloc(initial_size * sizeof(void *));
 	if (!buffer->data) return NULL;
 
-	buffer->len = initial_len;
+	buffer->size = initial_size;
 	buffer->end = 0;
 
 	return buffer;
@@ -66,16 +164,16 @@ int rtb_buffer_empty(rtb_buffer_t *buffer)
 	return !buffer->end;
 }
 
-int rtb_buffer_resize(rtb_buffer_t *buffer, size_t new_len)
+int rtb_buffer_resize(rtb_buffer_t *buffer, size_t new_size)
 {
 	assert(buffer);
-	assert(new_len > 0);
+	assert(new_size > buffer->size);
 
-	buffer->data = realloc(buffer->data, new_len * sizeof(void *));
+	buffer->data = realloc(buffer->data, new_size * sizeof(void *));
 	if (!buffer->data)
 		return 0;
 
-	buffer->len = new_len;
+	buffer->size = new_size;
 	return 1;
 }
 
@@ -83,9 +181,9 @@ int rtb_buffer_push(rtb_buffer_t *buffer, void *element)
 {
 	assert(buffer);
 
-	if (buffer->end == buffer->len) {
-		size_t new_len = buffer->len ? GROWTH_FACTOR * buffer->len : 1;
-		if (!rtb_buffer_resize(buffer, new_len))
+	if (buffer->end == buffer->size) {
+		size_t new_size = buffer->size ? buffer->size * RTB__GROWTH_FACTOR : 1;
+		if (!rtb_buffer_resize(buffer, new_size))
 			return 0;
 	}
 
